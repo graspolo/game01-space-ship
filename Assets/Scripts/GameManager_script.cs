bf3ab6ed4fc050b33fa989c2e61fb80be21cbfc2
Simone Mulas <66941363+graspolo@users.noreply.github.com>
2025-03-04 13:52:22 +0100

    Update GameManager_script.cs
    To be tested and complete the implementation.

--- Assets/Scripts/GameManager_script.cs
+++ Assets/Scripts/GameManager_script.cs
@@ -6,23 +6,33 @@ public class GameManager_script : MonoBehaviour
 
     public bool isGameOver = false;
     public bool isCoOpMode = false;
+    private bool _isGamePaused = false;
 
+    [SerializeObject]
+    private GameObject _pauseMenuPanel;
+
+    private void Start()
+    {
+        _isGamePaused = false;
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R) && isGameOver == true)
+        if (isGameOver == true)
         {
-            if (isCoOpMode == true)
-            {
-                SceneManager.LoadScene(2); //Co-Op Game Scene
-            }
-            else
-            {
-                SceneManager.LoadScene(1); //Main Game Scene
+            if (Input.GetKeyDown(KeyCode.R)) {
+                if (isCoOpMode == true)
+                {
+                    SceneManager.LoadScene(2); //Co-Op Game Scene
+                }
+                else
+                {
+                    SceneManager.LoadScene(1); //Main Game Scene
+                }
             }
             if (Input.GetKeyDown(KeyCode.Escape))
             {
-                SceneManager.LoadScene(0); //Main Menu Scene
+                GoToMainMenu(); //Main Menu Scene
             }
         }
 
@@ -30,10 +40,40 @@ public class GameManager_script : MonoBehaviour
         {
             Application.Quit();
         }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PauseGame();
+        }
     }
 
     public void GameOver()
     {
         isGameOver = true;
+    }
+
+    //TODO: invocare questo dal pannello di pausa
+    public void PauseGame()
+    {
+        if (_isGamePaused == false)
+        {
+            //TODO: implementare un oggetto UI Pause Menù
+            _pauseMenuPanel.SetActive(true);
+            _isGamePaused = true;
+            Time.TimeScale = 0f;
+        }
+        else
+        {
+            //TODO: implementare un oggetto UI Pause Menù
+            _pauseMenuPanel.SetActive(false);
+            _isGamePaused = false;
+            Time.timeScale = 1f;
+        }
+    }
+    
+    //TODO: invocare questo dal pannello di pausa
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
